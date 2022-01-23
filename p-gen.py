acc413fec4a4eed4dbf27e37822cb1d8e18909f8
AnonyMux037 <94628358+AnonyMux037@users.noreply.github.com>
2022-01-23 17:55:39 +0530

    Add files via upload

---
+++ p-gen.py
@@ -0,0 +1,38 @@
+import random
+# colors
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+# code
+print (red+b+"""
+         ██████╗        ██████╗ ███████╗███╗   ██╗
+         ██╔══██╗      ██╔════╝ ██╔════╝████╗  ██║
+         ██████╔╝█████╗██║  ███╗█████╗  ██╔██╗ ██║
+         ██╔═══╝ ╚════╝██║   ██║██╔══╝  ██║╚██╗██║
+         ██║           ╚██████╔╝███████╗██║ ╚████║
+         ╚═╝            ╚═════╝ ╚══════╝╚═╝  ╚═══╝
+                                                   v 1.0
+"""+b+red)
+
+print (gren+b+"              <===[[ coded by AnonyMux ]]===>"+b+gren)
+print (" ")
+print (yellow+b+"        <---( YOUTUBE- AnonyMux )--->"+b+yellow)
+print (" ")
+
+length=int(input(cyan+b+"Enter The Length Of The Password: "+b+cyan))
+print (" ")
+print (yellow+b+"-----> password  generated <----"+b+yellow)
+print (" ")
+char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+×÷=/_€£₹₩!@#$%^&*()-:;?`~\|<>{}[]"
+password= (gren+b+" "+b+gren)
+for i in range(length):
+
+     password+=random.choice(char)
+
+print(password)
+print (" ")
+print (yellow+b+"-----> grab your password <----"+b+yellow)
+print (" ")
